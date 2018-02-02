43769787ba4b5fab1488bb4cf04c9d1545c84d46
Evan Sharp <dumbdrum@gmail.com>
2018-02-02 13:59:44 -0500

    fix(MatchValidation): allow dot notation (#62)
    Closes #58

--- src/AvValidator/match.js
+++ src/AvValidator/match.js
@@ -1,5 +1,6 @@
 import { isEmpty } from './utils';
+import get from 'lodash/get';
 
 export default function validate(value, context, constraint = {}) {
-  return isEmpty(value) || value === context[constraint.value] || constraint.errorMessage || false;
+  return isEmpty(value) || value === get(context, constraint.value) || constraint.errorMessage || false;
 }
