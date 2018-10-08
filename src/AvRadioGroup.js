e5336d660c89f7e2d5620d4ed3e40c1ee47f3ed8
Evan Sharp <evan.sharp@availity.com>
2018-10-08 11:55:43 -0400

    fix(AvRadioGroup): check validation for required value

--- src/AvRadioGroup.js
+++ src/AvRadioGroup.js
@@ -56,7 +56,7 @@ export default class AvRadioGroup extends Component {
         getProps: () => ({
           name: this.props.name,
           inline: this.props.inline,
-          required: this.props.required,
+          required: this.props.required || !!(this.validations.required && this.validation.required.value),
           value: this.value,
         }),
         update: updateGroup,
