9b7815a7abeb0c9f290849c1e0fffed8c6ad79b2
Rob McGuinness <rob.mcguinness@availity.com>
2019-06-14 10:09:57 -0400

    Revert "refactor: replaced moment with dayjs"
    This reverts commit f5a9ffa435f0036df0c25dc0171ec4334ff6fe45.

--- src/AvValidator/max.js
+++ src/AvValidator/max.js
@@ -1,10 +1,7 @@
-import dayjs from 'dayjs';
-import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
+import moment from 'moment';
 import toNumber from 'lodash/toNumber';
 import { isEmpty, isoDateFormat } from './utils';
 import maxchecked from './maxchecked';
-
-dayjs.extend(isSameOrBefore);
 
 export default function validate(value, context, constraint = {}, input = {}) {
   if (Array.isArray(input.value)) {
@@ -15,7 +12,7 @@ export default function validate(value, context, constraint = {}, input = {}) {
 
   if ((input.validations && input.validations.date) ||
     (input.props && input.props.type && input.props.type.toLowerCase() === 'date')) {
-    return dayjs(value, [isoDateFormat, constraint.format || 'MM/DD/YYYY'], true).isSameOrBefore(constraint.value, 'day') || constraint.errorMessage || false;
+    return moment(value, [isoDateFormat, constraint.format || 'MM/DD/YYYY'], true).isSameOrBefore(constraint.value, 'day') || constraint.errorMessage || false;
   }
 
   const number = toNumber(value);
